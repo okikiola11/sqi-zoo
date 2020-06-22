dc147d8cec44a724b9b016d7f1bb618a6f4b8dee
Okikiola Apelehin <okikiola.apelehin@gmail.com>
2020-06-22 22:25:23 +0000

    Update the select from nobel queries challenge

--- 3_select_from_nobel.sql
+++ 3_select_from_nobel.sql
@@ -100,3 +100,18 @@ from nobel
 where winner like 'Sir%' 
 order by yr desc, winner
 
+
+/***************************
+he expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
+
+Show the 1984 winners and subject ordered by subject and winner name; 
+but list Chemistry and Physics last.
+***************************/
+SELECT winner, subject
+  FROM nobel
+  WHERE yr=1984
+  ORDER BY 
+  CASE WHEN subject IN ('Physics','Chemistry')
+  THEN 1
+  ELSE 0
+  END, subject,winner
